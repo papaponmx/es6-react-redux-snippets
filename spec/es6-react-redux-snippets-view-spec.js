12d522ee83dae07f7c9adade56f15052c3b0d045
Jaime Rios <jaime.madrigal@amk-technologies.com>
2017-10-21 18:53:00 -0500

    Basic structure

---
+++ spec/es6-react-redux-snippets-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import Es6ReactReduxSnippetsView from '../lib/es6-react-redux-snippets-view';
+
+describe('Es6ReactReduxSnippetsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
