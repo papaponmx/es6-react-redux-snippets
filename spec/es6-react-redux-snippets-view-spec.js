2d4f5d86373cc77d925e0683aae0ba7629dc2077
Jaime Rios <jaimemadrigalrios@gmail.com>
2017-12-10 20:15:37 -0600

    Rewrite.

--- spec/es6-react-redux-snippets-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import Es6ReactReduxSnippetsView from '../lib/es6-react-redux-snippets-view';
-
-describe('Es6ReactReduxSnippetsView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
